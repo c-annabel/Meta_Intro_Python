b966e103df1ddc730e4d5e3f912407aade12cc7a
c-annabel <106787550+c-annabel@users.noreply.github.com>
2022-09-14 20:40:42 +0200

    Maps & Filters

---
+++ 18_maps_filters.py
@@ -0,0 +1,18 @@
+menu = ['expresso', 'mocha', 'latte', 'cappuccino', 'cortado', 'americano']
+
+def find_coffee(coffee):
+    if coffee[0] == 'c':
+        return coffee
+
+#map
+map_coffee = map(find_coffee, menu)
+print(map_coffee)
+for x in map_coffee:
+    print(x)
+
+#filter
+filter_coffee = filter(find_coffee, menu)
+print(filter_coffee)
+for x in filter_coffee:
+    print(x)
+
