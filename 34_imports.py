002bb2da0a1a4278e7084235b15236884bf888cb
c-annabel <106787550+c-annabel@users.noreply.github.com>
2022-09-23 17:30:44 +0200

    Imports

---
+++ 34_imports.py
@@ -0,0 +1,19 @@
+# from math import sqrt
+#import math as m
+#from math import *
+from math import factorial as f
+from math import log10, sqrt
+
+# cosine = cos(0)
+# print(cosine)
+
+print("Importing the math module")
+
+root = sqrt(9)
+print(root)
+
+factorial_10 = f(10)
+print(factorial_10)
+
+x = log10(50)
+print(x)
